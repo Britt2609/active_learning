91e38f1e2c1fdabe152f96bc9778817a74a330d4
Britt0508 <britt2609@hotmail.com>
2023-05-04 14:09:13 +0200

    minor change

--- active/preprocess.py
+++ active/preprocess.py
@@ -10,7 +10,7 @@ import os
 import datetime
 from sklearn import metrics
 
-MAIN_PATH = "C:/Users/Gebruiker/PycharmProjects/active_learning_zeek/Etiennes_data"
+MAIN_PATH = "/content/active_learning/Etiennes_data"
 
 
 def get_labels():
